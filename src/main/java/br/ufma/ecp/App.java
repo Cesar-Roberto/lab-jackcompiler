a448f2d03ef2484976a8894a7fd5e07c832b52ea
Cesar-Roberto <cesar.robert1996@gmail.com>
2024-11-28 20:57:08 -0300

    ADD: Adição de keywords e funcionalidades dos comentários

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -1,44 +1,70 @@
 package br.ufma.ecp;
 
+/*
+ * Autores:
+ * Ana Júlia Morais Ribeiro Alencar
+ * Ailton Kayky Cordeiro da Silva
+ * César Roberto Ferreira Cardoso Filho
+ */
+
 import static br.ufma.ecp.token.TokenType.*;
 
+import br.ufma.ecp.token.Token;
 
+public class App {
 
-import br.ufma.ecp.token.Token; 
+    public static void main(String[] args) {
 
-public class App 
-{
+        
+        String input = """
+        // é um comentario 10
+        45 \"hello\" variavel + while < , if
+        /*
+        comentario em bloco
+        */
+        42 ola
 
-    
-    public static void main( String[] args )
-    {
-
-    
-        String input = "45  + if - \"ola\" laranja 876";
+        """;
         Scanner scan = new Scanner (input.getBytes());
+        
+        System.out.println("<tokens>");
         for (Token tk = scan.nextToken(); tk.type != EOF; tk = scan.nextToken()) {
             System.out.println(tk);
         }
+        System.out.println("</tokens>");
+
+        // String input = """
+        // // é um comentario 10
+        // 45 \"hello\" variavel + while < , if
+        // /*
+        // comentario em bloco
+        // */
+        // 42 ola
+
+        // """;
+        // Scanner scan = new Scanner (input.getBytes());
+        // for (Token tk = scan.nextToken(); tk.type != EOF; tk = scan.nextToken()) {
+        // System.out.println(tk);
+        // }
 
         /*
-        Parser p = new Parser (input.getBytes());
-        p.parse();
-        */
+         * Parser p = new Parser (input.getBytes());
+         * p.parse();
+         */
 
-
-        //Parser p = new Parser (fromFile().getBytes());
-        //p.parse();
+        // Parser p = new Parser (fromFile().getBytes());
+        // p.parse();
 
         /*
-        String input = "489-85+69";
-        Scanner scan = new Scanner (input.getBytes());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        Token tk = new Token(NUMBER, "42");
-        System.out.println(tk);
-        */
+         * String input = "489-85+69";
+         * Scanner scan = new Scanner (input.getBytes());
+         * System.out.println(scan.nextToken());
+         * System.out.println(scan.nextToken());
+         * System.out.println(scan.nextToken());
+         * System.out.println(scan.nextToken());
+         * System.out.println(scan.nextToken());
+         * Token tk = new Token(NUMBER, "42");
+         * System.out.println(tk);
+         */
     }
 }
