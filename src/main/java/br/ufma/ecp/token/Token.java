6cbd41361d454039753998014a5b9976e47e8620
Cesar-Roberto <cesar.robert1996@gmail.com>
2024-11-28 20:55:02 -0300

    ADD: Adição de novos simbolos no método toString

--- src/main/java/br/ufma/ecp/token/Token.java
+++ src/main/java/br/ufma/ecp/token/Token.java
@@ -3,19 +3,22 @@ public class Token {
 
     public final TokenType type;
     public final String lexeme;
+    final int line;
 
-    public Token (TokenType type, String lexeme) {
+    public Token (TokenType type, String lexeme, int line) {
         this.type = type;
         this.lexeme = lexeme;
+        this.line = line;
     }
 
     public String toString() {
         var type = this.type.toString();
+        String valor = lexeme;
         if (type.equals("NUMBER"))
-            type =  "intConst";
+            type =  "integerConstant";
 
         if (type.equals("STRING"))
-            type =  "stringConst";
+            type =  "stringConstant";
 
         if (type.equals("IDENT"))
             type =  "identifier";
@@ -23,11 +26,22 @@ public class Token {
         if (TokenType.isSymbol(lexeme.charAt(0)))
             type = "symbol";
 
+            //Os símbolos <, >, ", e & são impressos como &lt;  &gt;  &quot; e &amp; Para não conflitar com o significado destes símbolos no XML
+            if (valor.equals(">")) {
+                valor = "&gt;" ;
+            } else if (valor.equals("<")) {
+                valor = "&lt;" ;
+            } else if (valor.equals("\"")) {
+                valor = "&quot;" ;
+            } else if (valor.equals("&")) {
+                valor = "&amp;" ;
+            }
+
         if (TokenType.isKeyword(this.type) )
             type = "keyword";
     
 
-        return "<"+ type +">" + lexeme + "</"+ type + ">";
+        return "<"+ type +"> " + valor + " </"+ type + ">";
     }
     
 }
